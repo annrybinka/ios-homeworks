22c1ed0d3c16444492d8c6b5ee4b192119c716bc
Anna Zhukova <an.zhukova@mail.ru>
2023-03-07 00:10:22 +0300

    HW 1.3

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        // загрузить вью в память
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+        print("viewDidLoad")
+        
+        let button: UIButton = .init(type: .system)
+        button.setTitle("Кнопка", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // вью располагается по заданным параметрам
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+
 
 
 }
